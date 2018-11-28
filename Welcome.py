c9023043b33eac0cb1b4e41b94040dc85f7361f1
Jyoti Patil <Jyotip@cdac.in>
2018-11-28 18:12:02 +0530

    try to get reageon in which text is present

--- Welcome.py
+++ Welcome.py
@@ -1,7 +1,15 @@
 from PIL import Image
 import pytesseract
-pytesseract.pytesseract.tesseract_cmd = 'C:/Tesseract-OCR/tesseract'
-print(pytesseract.image_to_string(Image.open("D:/Jyoti-backup/python-project_folders/Info-Extraction/img1.jpg")))
+# pytesseract.pytesseract.tesseract_cmd = 'C:/Tesseract-OCR/tesseract'
+# print(pytesseract.image_to_string(Image.open("D:/Jyoti-backup/python-project_folders/Info-Extraction/mixed.png")))
 
-# pytesseract.pytesseract.run_tesseract('D:/Jyoti-backup/python-project_folders/Info-Extraction/eng2.jpg', 'output', lang=None, extension="hocr" )
+# pytesseract.pytesseract.run_tesseract('D:/Jyoti-backup/python-project_folders/Info-Extraction/eng2.jpg', 'out1', lang=None, extension="hocr")
 # print(pytesseract.image_to_string(Image.open("D:/Jyoti-backup/python-project_folders/Info-Extraction/hindi.jpg"), lang="hin"))
+
+
+import codecs
+import pyocr.builders
+
+builder = pyocr.builders.LineBoxBuilder()
+with codecs.open("out1.html", 'r', encoding='utf-8') as file_descriptor:
+    line_boxes = builder.read_file(file_descriptor)
--- hocr-lines.py
+++ hocr-lines.py
@@ -1,18 +1,23 @@
+#!/usr/bin/env python
+
+# extract the text within all the ocr_line elements within the hOCR file
+
 from __future__ import print_function
 import argparse
 import re
 import sys
 
 from lxml import html
+# this code is for parsing te file which is accepted from cmd
+# parser = argparse.ArgumentParser(
+#     description=('extract the text within all the ocr_line elements '
+#                  'within the hOCR file')
+# )
+# parser.add_argument('file', nargs='?', default=sys.stdin)
+# args = parser.parse_args()
 
-parser = argparse.ArgumentParser(
-    description=('extract the text within all the ocr_line elements '
-                 'within the hOCR file')
-)
-parser.add_argument('output.hocr', nargs='?', default=sys.stdin)
-args = parser.parse_args()
-print("vjvhfvh ", args)
-doc = html.parse(args)
 
-for line in doc.xpath("//*[@class='ocr_line']"):
+doc = html.parse("D:/Jyoti-backup/PyCharmProjects/Info-Extraction/output.hocr")
+
+for line in doc.xpath("//*[@class='ocr_par']"):
     print(re.sub(r'\s+', '\x20', line.text_content()).strip())
